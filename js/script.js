6422a46c59a4544271f5f91f38817895783c3221
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-26 13:47:31 -0400

    Refactored the apply button fuctionality so is less tedius to add effects. Added all the effect functions to an array.

--- js/script.js
+++ js/script.js
@@ -4,26 +4,27 @@ $(document).ready(function() {
     var $applyBtn = $("#applyBtn");
     var $resetBtn = $("#resetBtn");
     var $liText = "";
+    var $effectsArr = [
+        function draggableFx() {
+            $dummy.draggable({
+                containment: ".playground",
+            });
+        },
 
-    function draggableFx() {
-        $dummy.draggable({
-            containment: ".playground",
-        });
-    };
+        function resizableFx() {
+            $dummy.resizable({
+                containment: ".playground",
+            });
+        },   
+        
+        function bounceFx() {
+            $dummy.toggle( "bounce", { times: 3 }, "slow" );
+        },
 
-    function resizableFx() {
-        $dummy.resizable({
-            containment: ".playground",
-        });
-    };   
-    
-    function bounceFx() {
-        $dummy.toggle( "bounce", { times: 3 }, "slow" );
-    };
-
-    function clipFx() {
-        $dummy.toggle( "clip" );
-    };
+        function clipFx() {
+            $dummy.toggle( "clip" );
+        },
+    ];    
 
     function reseting() {
         var $dummyClass = $dummy.attr("class");
@@ -43,33 +44,41 @@ $(document).ready(function() {
         reseting();        
     });
 
+    // This code changes the content of the Dropdown selector button according to the choice made in the menu.
+    // Also sets the variable $liText that will allow the apply button to apply the selected effect.
     $("li").click(function() {
-        // This code changes the content of the Dropdown selector button according to the choice made in the menu.
-        // Also sets the variable $liText that will allow the apply button to apply the selected effect.
         $liText = $(this).text();
         $effectSelector.children().first().text($liText);
+        $liText = $liText.toLowerCase() + "Fx";
     });
 
+    //My first implementation was a switch function (see the code commented below) but once I realize how tedious was to add new effects to the list, I decided to add all effects into an Array 
+    //and thenmake the apply button loop trough the array to find if there is a function with the same name as the $liText variable. This way, no matter how may effects you add to the array,
+    //if you maintain the same "naming rule" for the fuction (same text than the li menu item  + Fx) the apply button will always work. So to add a new effect, you just have to write the fuction
+    //into the array and add the <li> item in the dropdown menu following the naming rule for the function.
     $applyBtn.click(function() {
-        switch ($liText) {
-            case "Resizable": 
-                resizableFx();
+        for (var i = 0; i < $effectsArr.length; i++) {
+            if ($effectsArr[i].name == $liText) {
+                $effectsArr[i]();
                 break;
-            case "Draggable":
-                draggableFx();
-                break;
-            case "Bounce":
-                bounceFx();
-                break;
-            case "Clip":
-                clipFx();
-                break;
-            case "Bounce":
-                bounceFx();
-                break;
-            case "Bounce":
-                bounceFx();
-                break;               
-        };
+            };
+        }
     });
+
+    // $applyBtn.click(function() {
+    //     switch ($liText) {
+    //         case "Resizable": 
+    //             resizableFx();
+    //             break;
+    //         case "Draggable":
+    //             draggableFx();
+    //             break;
+    //         case "Bounce":
+    //             bounceFx();
+    //             break;
+    //         case "Clip":
+    //             clipFx();
+    //             break;         
+    //     };
+    // });
 });
