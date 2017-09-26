9b058800c2d1475dd80008f4b31e1ba13276937c
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-25 20:01:51 -0400

    Added functionality to the buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,58 @@
 $(document).ready(function() {
-    $(".dummy").draggable({
-        containment: ".playground"
+    var $effectSelector = $("#effectSelector");
+    var $dummy = $(".dummy");
+    var $applyBtn = $("#applyBtn");
+    var $resetBtn = $("#resetBtn");
+
+    function draggableFx() {
+        $applyBtn.click(function() {
+            $dummy.draggable({
+                containment: ".playground",
+            });
+        });
+    };
+
+    function resizableFx() {
+        $applyBtn.click(function() {
+            $dummy.resizable({
+                containment: ".playground",
+            });
+        });
+    };   
+    
+    function reseting() {
+        var $dummyClass = $dummy.attr("class");
+
+        if ($dummyClass !== "dummy") {
+            $dummy.parent().html('<div class="dummy"></div>');
+        };
+        $dummy = $(".dummy");
+    };
+
+    function effectFx() {
+        
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    $resetBtn.click(function() {
+        reseting();        
+    });
+
+    $("li").click(function() {
+        // This code changes the content of the Dropdown selector button according to the choice made in the menu.
+        var $liText = $(this).text();
+        $effectSelector.children().first().text($liText);
+        //
+        switch ($liText) {
+            case "Resizable": 
+                resizableFx();
+                break;
+            case "Draggable":
+                draggableFx();
+                break;
+            default:
+                alert("default");
+        };
+
     });
 });
