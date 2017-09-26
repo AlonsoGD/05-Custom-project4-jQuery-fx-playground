6951b2b9dbc4c98e7024302d042161dc566f29d8
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-09-26 19:43:53 -0400

    Added swich functionality

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@ $(document).ready(function() {
     var $applyBtn = $("#applyBtn");
     var $resetBtn = $("#resetBtn");
     var $liText = "";
-    //This is the array where I store all the effects that are going to be usable. Later I explain why I did it this way.
+    var $resizableSwitch = $("#resizableSwitch");
+    var $drageableSwitch = $("#drageableSwitch");
+    //This is the array where I store all the effects that are going to be usable. Later I explain why I did it in this way.
     var $effectsArr = [
         function draggableFx() {
             $dummy.draggable({
                 containment: ".playground",
+                disabled: false,
             });
         },
 
         function resizableFx() {
             $dummy.resizable({
                 containment: ".playground",
+                disabled: false,
             });
         },   
-        
+               
         function bounceFx() {
             $dummy.toggle( "bounce", { times: 3 }, "slow" );
         },
-
+        
         function clipFx() {
             $dummy.toggle( "clip" );
         },
@@ -57,18 +61,43 @@ $(document).ready(function() {
         function slideFx() {
             $dummy.slideToggle( "slow" );
         },
-    ];    
+    ];
 
     function reseting() {
         var $dummyClass = $dummy.attr("class");
-        if ($dummyClass !== "dummy") {
-            $dummy.parent().html('<div class="dummy"></div>');
+        var $dummyStyle = $dummy.css("display");
+        if ($dummyClass !== "dummy" || $dummyStyle === "none") { //This "or" opperator let reset button work if a effect has set the display css property hidden the div from the screen
+            $dummy.parent().html('<div class="dummy"></div>'); //it removes the current "dummy" div and creates a new one with the css class dummy. So it effectively reset its position and style
         };
-        $dummy = $(".dummy");
+        $dummy = $(".dummy"); //refresh the $dummy variable to the new div created by the function
+        $drageableSwitch.prop("checked", false); //set the swich buttons to off
+        $resizableSwitch.prop("checked", false); //set the swich buttons to off
     };
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     $resetBtn.click(function() {
         reseting();        
+    });
+    
+    //This code is for the 2 switch buttons functionality
+    $resizableSwitch.click(function() {
+        if ($resizableSwitch.prop("checked") === false) {
+            $dummy.resizable({
+                disabled: true,
+            });
+        } else {
+            $effectsArr[1]();
+        }
+    });
+
+    $drageableSwitch.click(function() {
+        if ($drageableSwitch.prop("checked") === false) {
+            $dummy.draggable({
+                disabled: true,
+            });
+        } else {
+            $effectsArr[0]();
+        }
     });
 
     // This code changes the content of the Dropdown selector button according to the choice made in the menu.
@@ -83,14 +112,15 @@ $(document).ready(function() {
     //and thenmake the apply button loop trough the array to find if there is a function with the same name as the $liText variable. This way, no matter how may effects you add to the array,
     //if you maintain the same "naming rule" for the fuction (same text than the li menu item  + Fx) the apply button will always work. So to add a new effect, you just have to write the fuction
     //into the array and add the <li> item in the dropdown menu following the naming rule for the function.
-    $applyBtn.click(function() {
+    $applyBtn.click(function() {     
         for (var i = 0; i < $effectsArr.length; i++) {
             if ($effectsArr[i].name === $liText) {
                 $effectsArr[i]();
                 break;
             };
-        }
+        }       
     });
+});
 
     // $applyBtn.click(function() {
     //     switch ($liText) {
@@ -108,4 +138,3 @@ $(document).ready(function() {
     //             break;         
     //     };
     // });
-});
